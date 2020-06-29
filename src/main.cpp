b4605afa350fd210739c01b6f65bc980c1826ad7
wecassidy <wec.notif.github@gmail.com>
2020-06-29 09:05:44 -0700

    Refactor tape sensor error to a function

--- src/main.cpp
+++ src/main.cpp
@@ -52,6 +52,29 @@ void printPDParameters(bool running, unsigned int error, unsigned int kp,
   display.display();
 }
 
+int tapeSensorError(unsigned int tapeLeftReading, unsigned int tapeRightReading, int lastError) {
+  bool tapeLeftOn = tapeLeftReading > TAPE_ON_MIN;
+  bool tapeRightOn = tapeRightReading > TAPE_ON_MIN;
+
+  if (tapeLeftOn && tapeRightOn) {
+    return 0;
+  } else if (!tapeLeftOn && tapeRightOn) {
+    return -ERROR_ONE_OFF;
+  } else if (tapeLeftOn && !tapeRightOn) {
+    return ERROR_ONE_OFF;
+  } else {
+    // In the fully off the tape case, assume it's off on the same
+    // side as the last error. Implementation detail: if the robot
+    // goes from on tape to fully off tape in one iteration, assume it
+    // is on the right side of the tape.
+    if (lastError < 0) {
+      return -ERROR_BOTH_OFF;
+    } else {
+      return ERROR_BOTH_OFF;
+    }
+  }
+}
+
 void setup() {
   setupDisplay();
 
@@ -68,41 +91,23 @@ void loop() {
   unsigned int kp = analogRead(P_PIN);
   unsigned int kd = analogRead(D_PIN);
   unsigned int gain = analogRead(GAIN_PIN);
-  int sp = 0;
 
-  bool tapeLeftOn = analogRead(TAPE_L) > TAPE_ON_MIN;
-  bool tapeRightOn = analogRead(TAPE_R) > TAPE_ON_MIN;
+  unsigned int tapeLeft = analogRead(TAPE_L);
+  unsigned int tapeRight = analogRead(TAPE_R);
 
-  int error;
-  if (tapeLeftOn && tapeRightOn) {
-    error = 0;
-  } else if (!tapeLeftOn && tapeRightOn) {
-    error = -ERROR_ONE_OFF;
-  } else if (tapeLeftOn && !tapeRightOn) {
-    error = ERROR_ONE_OFF;
-  } else {
-    // In the fully off the tape case, assume it's off on the same
-    // side as the last error. Implementation detail: if the robot
-    // goes from on tape to fully off tape in one iteration, assume it
-    // is on the right side of the tape.
-    if (lastError < 0) {
-      error = -ERROR_BOTH_OFF;
-    } else {
-      error = ERROR_BOTH_OFF;
-    }
-  }
+  int error = tapeSensorError(tapeLeft, tapeRight, lastError);
 
   if (!digitalRead(PRINT_PIN)) {
     switchedLastIteration = true;
     motorL.setSpeed(0);
     motorR.setSpeed(0);
 
-    printPDParameters(true, error, kp, kd, gain);
+    printPDParameters(false, error, kp, kd, gain);
   } else {
     if (switchedLastIteration) {
       switchedLastIteration = false;
 
-      printPDParameters(false, error, kp, kd, gain);
+      printPDParameters(true, error, kp, kd, gain);
     }
 
     int p = kp * error;
